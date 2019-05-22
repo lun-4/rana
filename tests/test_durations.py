02460c264fbe0ed5f0b8c2291e12416cf4da7ec2
Luna <git@l4.pm>
2019-05-22 17:15:05 -0300

    fix duration test

--- tests/test_durations.py
+++ tests/test_durations.py
@@ -1,6 +1,6 @@
 import time
 import datetime
-import secrets
+import dateutil.parser
 
 import pytest
 
@@ -28,6 +28,8 @@ async def test_heartbeats(test_cli_user):
 async def test_durations(test_cli_user):
     """Test if given"""
     # create 10 minutes duration manually
+    start = datetime.datetime.now()
+
     for n in range(10):
         resp = await test_cli_user.post(
             '/api/v1/users/current/heartbeats', json={
@@ -40,6 +42,8 @@ async def test_durations(test_cli_user):
 
         assert resp.status_code == 201
 
+    end = datetime.datetime.now()
+
     utcnow = datetime.datetime.utcnow()
     now = f'{utcnow.year}-{utcnow.month}-{utcnow.day}'
     resp = await test_cli_user.get(
@@ -47,6 +51,18 @@ async def test_durations(test_cli_user):
 
     assert resp.status_code == 200
     rjson = await resp.json
-    print(rjson)
-    assert False
     assert isinstance(rjson, dict)
+
+    data = rjson['data']
+    assert isinstance(data, list)
+
+    duration = next(iter(data))
+    assert isinstance(duration, dict)
+    assert duration['project'] == 'awoo'
+
+    dstart = dateutil.parser.parse(duration['start'])
+    dend = dateutil.parser.parse(duration['end'])
+
+    # at least a 10min resolution
+    assert (dstart - start) < datetime.timedelta(seconds=600)
+    assert (dend - end) < datetime.timedelta(seconds=600)
